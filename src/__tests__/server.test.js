39d4d74fb1a6c0e37cd80bee74124ff568abb86a
Bhavesh Chavhan <97518972+Bhavesh299@users.noreply.github.com>
2022-06-20 22:55:25 +0530

    Add files via upload

--- src/__tests__/server.test.js
+++ src/__tests__/server.test.js
@@ -8,43 +8,104 @@ describe("Testing routing Application", () => {
     global.score = 0;
     done();
   });
+  afterAll((done) => {
+    done();
+  }
 
-  test("GET /", () => {
+  );
+  describe("GET /", () => {
+    it("should return a 200 response", (done) => {
+      supertest(app)
+        .get("/")
+        .expect(200, done);
+    }
+    );
+    it("should return a html file", (done) => {
+      supertest(app)
+        .get("/")
+        .expect("Content-Type", "text/html", done);
+    } 
+    );
+test("GET /", () => {
     supertest(app)
       .get("/")
       .expect(200)
       .then((response) => {
         let usersHtml = fs.readFileSync(__dirname + "../assets/users.html");
         // Check type and length
+        expect(typeof response.text).toBe("string");
+        expect(response.text.length).toBe(usersHtml.length);
+        // Check content
+        expect(response.text).toBe(usersHtml);
         expect(response.body).toBeTruthy();
         expect(response.body).toEqual(usersHtml);
       });
     global.score += 2;
   });
+  }
+  );
+  describe("GET /users", () => {
+    it("should return a 200 response", (done) => {
+      supertest(app)
+        .get("/users")
+        .expect(200, done);
+    }
+    );
+    it("should return a json file", (done) => {
+      supertest(app)
+        .get("/users")
+        .expect("Content-Type", "application/json", done);
+    }
+    );
+    
   test("GET /users", () => {
     supertest(app)
       .get("/users")
       .expect(200)
       .then((response) => {
         // Check type and length
+        expect(response.body).toBeTruthy();
+        expect(response.body).toEqual(users);
         expect(Array.isArray(response.body)).toBeTruthy();
         expect(response.body.length).toEqual(99);
       });
-    global.score += 2;
+    global.score += 2 ; 
   });
-
-  test("GET /users/1", () => {
+  }
+  );
+  describe("GET /users/:id", () => {
+    it("should return a 200 response", (done) => {
+      supertest(app)
+        .get("/users/1")
+        .expect(200, done);
+    }
+    );
+    it("should return a json file", (done) => {
+      supertest(app)
+        .get("/users/1")
+        .expect("Content-Type", "application/json", done);
+    }
+    );
+  test("GET /users/:id", () => {
     supertest(app)
       .get("/users/1")
       .expect(200)
       .then((response) => {
         // Check type and length
-        expect(response.body).toEqual(users[0]);
+        expect(response.body).toBeTruthy();
+        expect(response.body).toEqual(users[1]);
+        expect(response.body.id).toEqual(1);
       });
     global.score += 2;
   });
+  }
+  );
+
   afterAll((done) => {
     console.log("Final Score is", global.score);
     done();
   });
 });
+
+
+ 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 // install and import express
-const express = () => {};
-let app = express();
+const express = () => {} ; // eslint-disable-line
+let app = express() ; // create an instance of express
 
 // Code here
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "../assets/users.html");
+}
+);
+
+app.get("/users", (req, res) => {
+    res.json(users);
+}
+);
+
+app.get("/users/:id", (req, res) => {
+    res.json(users[req.params.id]);
+}
+);
+
+
+
+
 // Note: Do not remove this export statement
-module.exports = app;
+module.exports = app ; 
+ 
+ 
+ 
+
